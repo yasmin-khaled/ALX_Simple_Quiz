aed667da6a04758f0575ec5a096ffd7cfa1aecbc
Yasmin Khaled <yasmin.khaled.f@gmail.com>
2024-10-06 21:18:38 +0300

    adding calculator files

---
+++ calculator.js
@@ -0,0 +1,43 @@
+function add(number1 = 0, number2 = 0) {
+    return number1 + number2;
+}
+
+function subtract(number1 = 0, number2 = 0) {
+    return number1 - number2;
+}
+
+function multiply(number1 = 1, number2 = 1) {
+    return number1 * number2;
+}
+
+function divide(number1 = 0, number2 = 1) {
+    return number1 / number2;
+}
+
+document.getElementById('add').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = add(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+
+document.getElementById('subtract').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = subtract(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+
+document.getElementById('multiply').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = multiply(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+
+document.getElementById('divide').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = divide(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
